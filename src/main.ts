4baa963d38d0dfed55871db27686f6f52bcb4ec0
Андрей Канаев <andykanaev@yandex.ru>
2025-02-20 20:50:46 +0300

    Fixed cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // Локальный фронтенд
+      'https://proxy.luxe', // Продакшен-домен
+    ],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept',
+    credentials: true, // Для передачи кук и заголовков аутентификации
+  });
   // Swagger configuration
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
@@ -30,4 +38,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
